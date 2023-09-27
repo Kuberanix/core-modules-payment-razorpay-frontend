3b4e8b234da3bbb44e2a15c3596894f1e2b84536
Rakesh Dhariwal <rakeshdhariwal61@gmail.com>
2023-09-27 17:23:24 +0530

    fix/razpay provider

--- RazorPayProvider.jsx
+++ RazorPayProvider.jsx
@@ -1,4 +1,5 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
 export const RazorPayContext = createContext({
   isLoading: true,
   isError: true,
@@ -6,6 +7,26 @@ export const RazorPayContext = createContext({
 export default function Provider({ children }) {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const loader = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const res = await loadRazorpay();
+      if (!res) {
+        setIsError(true);
+        console.log('Razorpay error: Failed to load razorpay');
+        return;
+      }
+    } catch (error) {
+      setIsError(true);
+      console.log('Razorpay load error: ', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    loader();
+  }, [loader]);
   return (
     <RazorPayContext.Provider
       value={{ isLoading, isError, setIsLoading, setIsError }}
@@ -15,5 +36,5 @@ export default function Provider({ children }) {
   );
 }
 Provider.propTypes = {
-  children: PropTypes.element,
+  children: PropTypes.node.isRequired,
 };
